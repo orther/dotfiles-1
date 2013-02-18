96d8e73bff912141b790154aa1dd3c6293071f99
Aaron Jensen <aaronjensen@gmail.com>
2013-02-17 16:56:27 -0800

    add akracun-tmux tap

--- setup/brew.sh
+++ setup/brew.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 brew tap homebrew/dupes
+brew tap aaronjensen/tap
 
-brew install git qt ack git-flow macvim readline apple-gcc42 graphviz memcached reattach-to-user-namespace autoconf hub mongodb ssh-copy-id autojump imagemagick mysql the_silver_searcher automake tig bash-completion tmux jpeg pcre wemux ctags libevent phantomjs wget libtiff zsh postgresql
+brew install git qt ack git-flow macvim readline apple-gcc42 graphviz memcached reattach-to-user-namespace autoconf hub mongodb ssh-copy-id autojump imagemagick mysql the_silver_searcher automake tig bash-completion jpeg pcre wemux ctags libevent phantomjs wget libtiff zsh postgresql
+brew install --HEAD akracun-tmux
