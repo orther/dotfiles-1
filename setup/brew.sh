59c654d1a847c7b98a8bd4a815b623d638642c8c
Aaron Jensen <aaronjensen@gmail.com>
2013-12-07 13:06:19 -0800

    update brew

--- setup/brew.sh
+++ setup/brew.sh
@@ -2,4 +2,4 @@
 brew tap homebrew/dupes
 brew tap aaronjensen/tap
 
-brew install git qt ack git-flow macvim readline apple-gcc42 graphviz memcached reattach-to-user-namespace autoconf hub mongodb ssh-copy-id autojump imagemagick mysql the_silver_searcher automake tig bash-completion jpeg pcre wemux ctags libevent phantomjs wget libtiff zsh postgresql tmux
+brew install git qt ack git-flow macvim readline graphviz memcached reattach-to-user-namespace autoconf hub mongodb ssh-copy-id autojump imagemagick mysql the_silver_searcher automake tig bash-completion jpeg pcre wemux ctags libevent phantomjs wget libtiff zsh postgresql tmux
