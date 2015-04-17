f667d379c5dcf0e6209fcea096bae26664464e26
Aaron Jensen <aaronjensen@gmail.com>
2015-04-16 22:38:30 -0700

    try prezto

---
+++ setup-zsh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+./setup.sh
+git submodule init
+git submodule update
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
