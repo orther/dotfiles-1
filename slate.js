4de847c20c7e2f99b7acd3d29a7c4c8790ec0a21
Aaron Jensen <aaronjensen@gmail.com>
2015-05-08 00:03:21 -0700

    Only use slate.js

--- slate.js
+++ slate.js
@@ -1,6 +1,16 @@
+S.configAll({
+  "defaultToCurrentScreen" : true,
+  "checkDefaultsOnLoad" : true,
+  "orderScreensLeftToRight" : true
+});
+
 var hyper = "ctrl,cmd,alt,shift"
 
-var quickSwitchApps = {
+/*
+ * Quick app switching and launching
+ * hyper + key to launch app or switch to it.
+ */
+var quickSwitchBindings = {
   "a" : "Android Studio",
   "c" : "Google Chrome",
   "f" : "Firefox",
@@ -12,10 +22,101 @@ var quickSwitchApps = {
   "x" : "Mailbox (Beta)"
 };
 
-_.each(quickSwitchApps, function(app, key) {
+_.each(quickSwitchBindings, function(app, key) {
   var appPath = "'/Applications/" + app + ".app'"
+
   S.bind(key + ":" + hyper, function() {
-    S.op("focus", { "app" : app }).run();
-    S.shell("/usr/bin/open " + appPath);
+    S.op("focus", { "app" : app }).run() ||
+      S.shell("/usr/bin/open " + appPath);
   });
 });
+
+/*
+ * Size and move windows
+ * Press binding twice to toggle between screens
+ */
+var fullscreen = {
+  "x" : "screenOriginX",
+  "y" : "screenOriginY",
+  "width" : "screenSizeX",
+  "height" : "screenSizeY"
+};
+
+var left = { "width" : "screenSizeX / 2" };
+var topLeft = {
+  "width" : "screenSizeX / 2",
+  "height" : "screenSizeY / 2"
+};
+var top = { "height" : "screenSizeY / 2" };
+var topRight = { "height" : "screenSizeY / 2" };
+var right = {
+  "x" : "screenOriginX + screenSizeX / 2",
+  "width" : "screenSizeX / 2"
+};
+var bottomRight = {
+  "x" : "screenOriginX + screenSizeX / 2",
+  "y" : "screenOriginY + screenSizeY / 2",
+  "width" : "screenSizeX / 2",
+  "height" : "screenSizeY / 2"
+};
+var bottom = {
+  "x" : "screenOriginX + screenSizeX / 2",
+  "height" : "screenSizeY / 2"
+};
+var bottomLeft = {
+  "y" : "screenOriginY + screenSizeY / 2",
+  "width" : "screenSizeX / 2",
+  "height" : "screenSizeY / 2"
+}
+var rightThird = {
+  "x" : "screenOriginX + screenSizeX * 7 / 12",
+  "width" : "screenSizeX * 5 / 12"
+};
+var leftTwoThirds = { "width" : "screenSizeX * 7 / 12" };
+
+var windowSizeBindings = {
+  "i:ctrl,cmd" : fullscreen,
+  "h:ctrl,cmd" : left,
+  "h:ctrl,cmd,shift" : topLeft,
+  "k:ctrl,cmd" : top,
+  "k:ctrl,cmd,shift" : topRight,
+  "l:ctrl,cmd" : right,
+  "l:ctrl,cmd,shift" : bottomRight,
+  "j:ctrl,cmd" : bottom,
+  "j:ctrl,cmd,shift" : bottomLeft,
+  "l:ctrl,cmd,alt" : rightThird,
+  "h:ctrl,cmd,alt" : leftTwoThirds
+};
+
+_.each(windowSizeBindings, function(size, key) {
+  S.bind(key, function(win) {
+    if (!win) return false;
+
+    var origRect = win.rect();
+    var move = _.extend({}, fullscreen, size);
+
+    win.doOperation(S.op("move", move));
+
+    if (rectEq(origRect, win.rect())) {
+      move.screen = "next"
+      win.doOperation(S.op("move", move));
+    }
+  });
+});
+
+/*
+ * Miscellaneous bindings
+ */
+S.bindAll({
+  "`:ctrl" : S.op("relaunch")
+});
+
+/*
+ * Utility functions
+ */
+function rectEq(a, b) {
+  return a.x === b.x &&
+    a.y === b.y &&
+    a.width === b.width &&
+    a.height === b.height;
+}
