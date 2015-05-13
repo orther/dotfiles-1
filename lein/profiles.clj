ed4f07604d343b1319d2238183963d23f76b6450
Aaron Jensen <aaronjensen@gmail.com>
2015-05-13 10:12:24 -0700

    add more to lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,8 @@
-{:user {:dependencies [[pjstadig/humane-test-output "0.7.0"]]
+{:user {:dependencies [[pjstadig/humane-test-output "0.7.0"]
+                       [spyscope "0.1.5"]
+                       [criterium "0.4.3"]]
+        ; :global-vars (*warn-on-reflection* true)
         :injections [(require 'pjstadig.humane-test-output)
+                     (require 'spyscope.core)
                      (pjstadig.humane-test-output/activate!)]
         :plugins [[cider/cider-nrepl "0.8.2"]]}}
