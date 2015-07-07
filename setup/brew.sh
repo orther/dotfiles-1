4909665872726d2469589cdff64598ba6866e1a4
Aaron Jensen <aaronjensen@gmail.com>
2015-07-07 12:14:22 -0700

    Add tree

--- setup/brew.sh
+++ setup/brew.sh
@@ -4,47 +4,47 @@ brew install caskroom/cask/brew-cask
 brew tap thoughtbot/formulae
 brew update
 
-brew install \
-ack \
-autoconf \
-autojump \
-automake \
-bash \
-bash-completion \
-boot2docker \
-brew-cask \
-ctags \
-direnv \
-docker \
-fasd \
-gist \
-git \
-git-flow \
-graphviz \
-hub \
-imagemagick \
-jpeg \
-libevent \
-libtiff \
-memcached \
-mongodb \
-mysql \
-pcre \
-phantomjs \
-pick \
-postgresql \
-qt \
-readline \
-reattach-to-user-namespace \
-ssh-copy-id \
-terminal-notifier \
-the_silver_searcher \
-tig \
-tmux \
-urlview \
-wemux \
-wget \
-zsh
+brew install ack
+brew install autoconf
+brew install autojump
+brew install automake
+brew install bash
+brew install bash-completion
+brew install boot2docker
+brew install brew-cask
+brew install ctags
+brew install direnv
+brew install docker
+brew install fasd
+brew install gist
+brew install git
+brew install git-flow
+brew install graphviz
+brew install hub
+brew install imagemagick
+brew install jpeg
+brew install libevent
+brew install libtiff
+brew install memcached
+brew install mongodb
+brew install mysql
+brew install pcre
+brew install phantomjs
+brew install pick
+brew install postgresql
+brew install qt
+brew install readline
+brew install reattach-to-user-namespace
+brew install ssh-copy-id
+brew install terminal-notifier
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install tree
+brew install urlview
+brew install wemux
+brew install wget
+brew install zsh
 
 brew install macvim --with-cscope --with-luajit
 
