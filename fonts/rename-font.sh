21347c576fd5eb183247002e70f6cc38be935ce3
Aaron Jensen <aaronjensen@gmail.com>
2016-05-21 09:17:07 -0700

    Add more powerline fonts

---
+++ fonts/rename-font.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env fontforge
+Open("Inconsolata-g for Powerline.otf")
+SetFontNames("InconsolatagForPowerline", "Inconsolatag for Powerline", "Inconsolatag for Powerline", "", "", "")
+Generate("Inconsolatag for Powerline.otf", "", 4)
