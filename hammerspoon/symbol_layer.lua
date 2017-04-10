9bcf07ebaf3ef7f597c37daefaae78d5492e13a0
Aaron Jensen <aaronjensen@gmail.com>
2017-04-09 17:20:18 -0700

    Rewrite symbol layer using eventtap

--- hammerspoon/symbol_layer.lua
+++ hammerspoon/symbol_layer.lua
@@ -1,46 +1,7 @@
--- A global variable for the Symbol Mode
-k = hs.hotkey.modal.new()
-
+-- The default delay is too slow, making key repeating very slow
 fast_delay = 1000
--- If you hold capslock for this long and then let go, ESC will not be sent.
-cancel_delay_seconds = 0.3
 
-bindings = {
-  {'o', {}, '-'},         -- o -> -
-  {'p', {'shift'}, '='},  -- p -> +
-  {'s', {}, '['},         -- s -> [
-  {'d', {'shift'}, '['},  -- d -> {
-  {'f', {'shift'}, '9'},  -- f -> (
-  {'g', {}, '='},         -- g -> =
-  {'h', {'shift'}, '-'},  -- h -> _
-  {'j', {'shift'}, '0'},  -- j -> )
-  {'k', {'shift'}, ']'},  -- k -> }
-  {'l', {}, ']'},         -- l -> ]
-  {'c', {'shift'}, '`'},  -- c -> ~
-  {'v', {}, '`'},         -- v -> `
-  {'m', {'shift'}, '\\'}, -- m -> |
-  {',', {'shift'}, ','},  -- , -> <
-  {'.', {'shift'}, '.'},  -- . -> >
-}
-
-modifierCombinations = {}
-for _, ctrl in ipairs({'ctrl', false}) do
-  for _, cmd in ipairs({'cmd', false}) do
-    for _, shift in ipairs({'shift', false}) do
-      for _, alt in ipairs({'alt', false}) do
-        local combination = {}
-        if ctrl then table.insert(combination, ctrl) end
-        if cmd then table.insert(combination, cmd) end
-        if shift then table.insert(combination, shift) end
-        if alt then table.insert(combination, alt) end
-
-        table.insert(modifierCombinations, combination)
-      end
-    end
-  end
-end
-
-function contains(tbl, value)
+function tableContainsValue(tbl, value)
   for _, v in ipairs(tbl) do
     if v == value then
       return true
@@ -50,14 +11,14 @@ function contains(tbl, value)
   return false
 end
 
-function union(a, b)
+function itableUnion(a, b)
   local ab = {}
   for _, v in ipairs(a) do
     table.insert(ab, v)
   end
 
   for _, v in ipairs(b) do
-    if not contains(ab, v) then
+    if not tableContainsValue(ab, v) then
       table.insert(ab, v)
     end
   end
@@ -65,50 +26,94 @@ function union(a, b)
   return ab
 end
 
-function withAllModifiers(fn)
-  for _, modifiers in ipairs(modifierCombinations) do
-    fn(modifiers)
+function tableKeys(tbl)
+  local ret = {}
+  for key, _ in pairs(tbl) do
+    table.insert(ret, key)
   end
+
+  return ret
 end
 
-for i, mapping in ipairs(bindings) do
-  withAllModifiers(function(modifiers)
-    local strokeModifiers = union(modifiers, mapping[2])
-    k:bind(modifiers, mapping[1], function()
-      hs.eventtap.keyStroke(strokeModifiers, mapping[3], fast_delay)
-      k.triggered = true
-    end, nil, function()
-      hs.eventtap.keyStroke(strokeModifiers, mapping[3], fast_delay)
-    end)
+function enableSymbolLayer()
+  local symbols = {
+    o = {{}, '-'},            -- o -> -
+    p = {{'shift'}, '='},     -- p -> +
+    s = {{}, '['},            -- s -> [
+    d = {{'shift'}, '['},     -- d -> {
+    f = {{'shift'}, '9'},     -- f -> (
+    g = {{}, '='},            -- g -> =
+    h = {{'shift'}, '-'},     -- h -> _
+    j = {{'shift'}, '0'},     -- j -> )
+    k = {{'shift'}, ']'},     -- k -> }
+    l = {{}, ']'},            -- l -> ]
+    c = {{'shift'}, '`'},     -- c -> ~
+    v = {{}, '`'},            -- v -> `
+    m = {{'shift'}, '\\'},    -- m -> |
+    [','] = {{'shift'}, ','}, -- , -> <
+    ['.'] = {{'shift'}, '.'}, -- . -> >
+  }
+
+  -- If you hold capslock for this long and then let go, ESC will not be sent.
+  local cancel_delay_seconds = 0.3
+  local triggered = true
+  local resetTimer = hs.timer.delayed.new(cancel_delay_seconds, function()
+    triggered = true
   end)
-end
 
-symbolResetTimer = hs.timer.delayed.new(cancel_delay_seconds, function()
-  k.triggered = true
-end)
-
--- Enter Symbol Mode when F18 (Capslock) is pressed
-function pressedF18(modifiers)
-  return function()
-    symbolResetTimer:start()
-    k.triggered = false
-    k:enter()
-  end
-end
-
--- Leave Symbol Mode when F18 (Capslock) is pressed,
--- send ESCAPE if no other keys are pressed
-function releasedF18(modifiers)
-  return function()
-    symbolResetTimer:stop()
-    k:exit()
-    if not k.triggered then
-      hs.eventtap.keyStroke(modifiers, 'ESCAPE', fast_delay)
+  -- This tap handles remapping. It is only enabled while capslock is held
+  local tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
+    -- Check if this event is from the keyboard or from us. We need to ignore
+    -- events from us.
+    local processId = event:getProperty(hs.eventtap.event.properties.eventSourceUnixProcessID)
+    if processId > 0 then
+      return false
     end
-  end
+
+    triggered = true
+
+    local key = hs.keycodes.map[event:getKeyCode()]
+    local remap = symbols[key]
+    if remap then
+      local mods = tableKeys(event:getFlags())
+      local newMods = itableUnion(mods, remap[1])
+      local newKey = remap[2]
+
+      hs.eventtap.keyStroke(newMods, newKey, fast_delay)
+    end
+
+    -- Do not send event on
+    return true
+  end)
+
+  -- This tap handles F18 (capslock) with any modifier and is enabled right away.
+  hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, function(event)
+    local key = hs.keycodes.map[event:getKeyCode()]
+    -- Ignore all events but f18
+    if key ~= 'f18' then
+      return false
+    end
+
+    if event:getType() == hs.eventtap.event.types.keyDown then
+      resetTimer:start()
+      triggered = false
+      tap:start()
+    else
+      resetTimer:stop()
+      tap:stop()
+
+      if not triggered then
+        -- Strip the fn flag, which is set because the key is f18
+        local flags = event:getFlags()
+        flags['fn'] = nil
+        local mods = tableKeys(flags)
+        hs.eventtap.keyStroke(mods, 'ESCAPE', fast_delay)
+      end
+    end
+
+    -- Do not send event on
+    return true
+  end):start()
 end
 
--- Bind the Symbol key
-withAllModifiers(function(modifiers)
-  hs.hotkey.bind(modifiers, 'F18', pressedF18(modifiers), releasedF18(modifiers))
-end)
+enableSymbolLayer()
